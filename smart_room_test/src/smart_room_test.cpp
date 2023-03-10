e74285f58e7ce4880902122281f0643e5d11841f
ɨֆǟǟƈ ɖɛ ʟɛօռ <deleonisaac978@gmail.com>
2023-03-09 18:04:50 -0700

    detection device

--- smart_room_test/src/smart_room_test.cpp
+++ smart_room_test/src/smart_room_test.cpp
@@ -16,38 +16,38 @@ void setup();
 void loop();
 #line 9 "c:/Users/pstee/Documents/IoT/SmartRoomControler/smart_room_test/src/smart_room_test.ino"
 #define OLED_RESET -1
+
 Adafruit_SSD1306 display(OLED_RESET);
+
 const int BULB5=5;
 const int BULB4=4;
 const int BULB3=3;
 const int BULB2=2;
 const int BULB1=1;
-
-
-int irsensor= A2;
-int sensdata;
+const int button=D3;
+const int PIXELPIN = D8 ; // Pin the NeoPixels are connected to
+const int PIXELCOUNT = 16; // Total number of NeoPixels
 const int MYWEMO=4;
 
-const int button=D3;
+const int button2=A1;
+bool buttonState2; 
+bool toggler2;
+
+
+
+int color;
+int pixelnumber; 
+int irsensor= A2;
+int sensdata;
+
 bool buttonState; 
 bool toggler;
-int color;
-
-
- const int PIXELPIN = D8 ; // Pin the NeoPixels are connected to
- const int PIXELCOUNT = 16; // Total number of NeoPixels
-int pixelnumber; 
-
 bool up;
-
 
 Adafruit_NeoPixel pixel ( PIXELCOUNT , PIXELPIN , WS2812B ); 
 
-
-
-SYSTEM_MODE(SEMI_AUTOMATIC);  //MANUEL
-SYSTEM_THREAD(ENABLED);
-
+SYSTEM_MODE(MANUAL);  //MANUEL
+//SYSTEM_THREAD(ENABLED);
 
 void setup(){
   pixel . begin () ;
@@ -55,29 +55,18 @@ pixel . show () ;
 Wire.begin ();     
   Serial.begin(9600);
 
+     pinMode(A1,INPUT); 
 
-
-  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-
-
-    display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.printf("Secured");
-  display.setTextColor(BLACK, WHITE); // 'inverted' text
-  display.setTextSize(1);
+  display.clearDisplay();
+  display.display();
 
 pinMode (A2, INPUT);
-
      pinMode( D3, INPUT_PULLDOWN ) ; 
-     
     Serial.begin(9600);
   waitFor(Serial.isConnected,15000);
-
   WiFi.on();
   WiFi.setCredentials("IoTNetwork");
-
   WiFi.connect();
    while(WiFi.connecting()) {
      Serial.printf(".");
@@ -90,37 +79,91 @@ pinMode (A2, INPUT);
 
 
 void loop(){
+
+if (sensdata>3000){
+  // display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
  for (pixelnumber=0; pixelnumber<=PIXELCOUNT; pixelnumber++) {
-    pixel . setPixelColor (pixelnumber , green); 
- pixel . setBrightness (15) ;
-pixel . show () ;
+    pixel . setPixelColor (pixelnumber , red); 
+    pixel . setBrightness (30) ;
+    pixel . show () ;
+ }
+display.clearDisplay();
+display.setTextSize(1);
+display.setTextColor(WHITE);
+display.setCursor(0,0);
+display.printf("Unstable!");
+display.setTextColor(BLACK, WHITE); // 'inverted' text
+display.setTextSize(1);
+display.setTextColor(WHITE);
+display.setCursor(0,35);
+display.printf("tripped proceed with caution");
+display.display();      
+Serial.printf("***************************************************unstablw\n\n\n");
+    buttonState=digitalRead(button);
 
-sensdata=analogRead(irsensor);
-digitalWrite(A5,HIGH);
-Serial.printf("%i\r",sensdata);
 
-buttonState=digitalRead(button);
-if (buttonState){
-toggler=!toggler;
-}
-if(toggler){
-switchON(MYWEMO);
-setHue(BULB5,true,HueGreen,255,255);
-setHue(BULB1,true,HueGreen,255,255);
-setHue(BULB2,true,HueGreen,255,255);
-setHue(BULB3,true,HueGreen,255,255);
-setHue(BULB4,true,HueGreen,255,255);
-}
+    //switchON(MYWEMO);
+      setHue(BULB5,true,HueRed,255,255);
+      setHue(BULB1,true,HueRed,255,255);
+      setHue(BULB2,true,HueRed,255,255);
+      setHue(BULB3,true,HueRed,255,255);
+      setHue(BULB4,true,HueRed,255,255);
+    
+   
+
+ }
 else{
-//switchOFF(MYWEMO);
-setHue (BULB5,false,HueGreen,255,255);
-setHue (BULB1,false,HueGreen,255,255);
-setHue (BULB2,false,HueGreen,255,255);
-setHue (BULB3,false,HueGreen,255,255);
-setHue (BULB4,false,HueGreen,255,255);
+   for (pixelnumber=0; pixelnumber<=PIXELCOUNT; pixelnumber++) {
+    pixel . setPixelColor (pixelnumber , green); 
+    pixel . setBrightness (30) ;
+    pixel . show () ;
+ }
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.printf("Secured!");
+  display.setTextColor(BLACK, WHITE); // 'inverted' text
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,35);
+  display.printf("active and or idle");
+  display.display();
+  Serial.printf("*****************************************secured\n\n\n");
+  buttonState=digitalRead(button);
+     setHue(BULB5,true,HueGreen,255,255);
+      setHue(BULB1,true,HueGreen,255,255);
+      setHue(BULB2,true,HueGreen,255,255);
+      setHue(BULB3,true,HueGreen,255,255);
+      setHue(BULB4,true,HueGreen,255,255);
+              sensdata=analogRead(irsensor);
+
+    }
+
+    
+    sensdata=analogRead(irsensor);
+    Serial.printf("sensdata%d\n\n",sensdata);
+    digitalWrite(A5,HIGH);
+    Serial.printf("%i\r\n\n",sensdata);
+
+
+    buttonState2=digitalRead(button2);
+    if (buttonState2){
+      Serial.printf("\n\nbutton2down\n\n");
+     toggler2=!toggler2;
+    }
+    if(toggler2){
+      switchON(MYWEMO);
+      Serial.printf("\n\ntogglertrue\n\n"); 
+    }
+    else{
+      switchOFF(MYWEMO);
+      Serial.printf("\n\ntogglerfalse\n\n"); 
+
+    }
 }
 
-}
+ 
 
 
-}
+
